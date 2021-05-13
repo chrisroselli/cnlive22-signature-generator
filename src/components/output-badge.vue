8939dbe3d50812c4333e6a1a482814e910e98005
chrisroselli <croselli@contractornation.com>
2021-05-13 16:24:46 -0400

    updates

--- src/components/output-badge.vue
+++ src/components/output-badge.vue
@@ -35,7 +35,6 @@
             </td>
           </tr>
         </table>
-    
     </section>
   </div>
 </template>
--- src/components/output-signature.vue
+++ src/components/output-signature.vue
@@ -75,7 +75,6 @@
             </td>
           </tr>
         </table>
-    
     </section>
   </div>
 </template>
