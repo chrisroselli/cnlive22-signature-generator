468d5859655c00c99931b9e0c29dcfbd316eb4d4
chrisroselli <croselli@contractornation.com>
2021-05-14 14:07:50 -0400

    updates

--- src/components/output-signature.vue
+++ src/components/output-signature.vue
@@ -8,7 +8,7 @@
             "padding-top: 0; padding-bottom: 0; padding-left: 0; padding-right: 7px; border-top: 0; border-bottom: 0: border-left: 0; border-right: solid 2px #FF7F30">
             <a href="https://www.contractornationlive.com/"><img width="175"
                     height="96" style="width: 175px; max-width: 175px !important; height: 96px; max-height: 96px !important;" id="preview-image-url" src=
-            "https://cdn.treehouseinternetgroup.com/cms_images/872/CNLive_2021-sign-logo.jpg"
+            "https://cdn.treehouseinternetgroup.com/cms_images/872/CNLive_2020_sign_logo.jpg"
               name="preview-image-url" /></a>
             </td>
             
