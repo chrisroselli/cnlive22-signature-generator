4aebb88e59f74a95c2a4e6b3ebbbd075b9732238
chrisroselli <croselli@contractornation.com>
2022-03-28 11:44:21 -0400

    cnlive22 fixed src

--- src/components/output-badge.vue
+++ src/components/output-badge.vue
@@ -7,8 +7,7 @@
             <td valign="top" style=
             "padding-top: 0; padding-bottom: 0; padding-left: 0; padding-right: 7px; border-top: 0; border-bottom: 0: border-left: 0;">
             <a href="https://www.contractornationlive.com/"><img width="175"
-                    height="77" style="width: 175px; max-width: 175px !175important; height: 77px; max-height: 77px !important;" id="preview-image-url" src=
-            "https://cdn.treehouseinternetgroup.com/cms_images/2535/CNLive_2022_sign_badge.jpg"
+                    height="77" style="width: 175px; max-width: 175px !175important; height: 77px; max-height: 77px !important;" id="preview-image-url" src="https://cdn.treehouseinternetgroup.com/cms_images/2535/CNLive_2022_sign_badge.jpg"
               name="preview-image-url" /></a>
                 <tr>
                   <td colspan="2" style="padding: 5px 41px;">
@@ -16,20 +15,17 @@
                     "border-width:0px; border:0px; text-decoration: none;"><img width="20"
                     height="20" style=
                     "border: none; width: 25px; max-width: 25px !important; height: 25px; max-height: 25px !important;"
-                    src=
-                    "https://cdn.treehouseinternetgroup.com/cms_images/872/cnlive-fb.png" /></a>&nbsp;&nbsp;<a href="https://twitter.com/ContractorNatn" target="_blank"
+                    src="https://cdn.treehouseinternetgroup.com/cms_images/872/cnlive-fb.png" /></a>&nbsp;&nbsp;<a href="https://twitter.com/ContractorNatn" target="_blank"
                     style=
                     "border-width:0px; border:0px; text-decoration: none;"><img width="20"
                     height="20" style=
                     "border: none; width: 25px; max-width: 25px !important; height: 25px; max-height: 25px !important;"
-                    src=
-                    "https://cdn.treehouseinternetgroup.com/cms_images/872/cnlive-twitter.png" /></a>&nbsp;&nbsp;<a href="https://www.instagram.com/contractor.nation/" target="_blank"
+                    src="https://cdn.treehouseinternetgroup.com/cms_images/872/cnlive-twitter.png" /></a>&nbsp;&nbsp;<a href="https://www.instagram.com/contractor.nation/" target="_blank"
                     style=
                     "border-width:0px; border:0px; text-decoration: none;"><img width="20"
                     height="20" style=
                     "border: none; width: 25px; max-width: 25px !important; height: 25px; max-height: 25px !important;"
-                    src=
-                    "https://cdn.treehouseinternetgroup.com/cms_images/872/cnlive-instagram.png" /></a>
+                    src="https://cdn.treehouseinternetgroup.com/cms_images/872/cnlive-instagram.png" /></a>
                   </td>
                 </tr>
             </td>
@@ -38,6 +34,7 @@
     </section>
   </div>
 </template>
+
 <script>
 export default {
   name: 'outputBadge'
--- src/components/output-signature.vue
+++ src/components/output-signature.vue
@@ -7,8 +7,7 @@
             <td valign="top" style=
             "padding-top: 0; padding-bottom: 0; padding-left: 0; padding-right: 7px; border-top: 0; border-bottom: 0: border-left: 0; border-right: solid 2px #FF7F30">
             <a href="https://www.contractornationlive.com/"><img width="175"
-                    height="145" style="width: 175px; max-width: 175px !important; height: 145px; max-height: 145px !important;" id="preview-image-url" src=
-            "https://cdn.treehouseinternetgroup.com/cms_images/2535/CNLive_2022_sign_8.jpg"
+                    height="145" style="width: 175px; max-width: 175px !important; height: 145px; max-height: 145px !important;" id="preview-image-url" src="https://cdn.treehouseinternetgroup.com/cms_images/2535/CNLive_2022_sign_8.jpg"
               name="preview-image-url" /></a>
             </td>
             
@@ -68,20 +67,17 @@
                     "border-width:0px; border:0px; text-decoration: none;"><img width="20"
                     height="20" style=
                     "border: none; width: 25px; max-width: 25px !important; height: 25px; max-height: 25px !important;"
-                    src=
-                    "https://cdn.treehouseinternetgroup.com/cms_images/872/cnlive-fb.png" /></a>&nbsp;&nbsp;<a href="https://twitter.com/ContractorNatn" target="_blank"
+                    src="https://cdn.treehouseinternetgroup.com/cms_images/872/cnlive-fb.png" /></a>&nbsp;&nbsp;<a href="https://twitter.com/ContractorNatn" target="_blank"
                     style=
                     "border-width:0px; border:0px; text-decoration: none;"><img width="20"
                     height="20" style=
                     "border: none; width: 25px; max-width: 25px !important; height: 25px; max-height: 25px !important;"
-                    src=
-                    "https://cdn.treehouseinternetgroup.com/cms_images/872/cnlive-twitter.png" /></a>&nbsp;&nbsp;<a href="https://www.instagram.com/contractor.nation/" target="_blank"
+                    src="https://cdn.treehouseinternetgroup.com/cms_images/872/cnlive-twitter.png" /></a>&nbsp;&nbsp;<a href="https://www.instagram.com/contractor.nation/" target="_blank"
                     style=
                     "border-width:0px; border:0px; text-decoration: none;"><img width="20"
                     height="20" style=
                     "border: none; width: 25px; max-width: 25px !important; height: 25px; max-height: 25px !important;"
-                    src=
-                    "https://cdn.treehouseinternetgroup.com/cms_images/872/cnlive-instagram.png" /></a>
+                    src="https://cdn.treehouseinternetgroup.com/cms_images/872/cnlive-instagram.png" /></a>
                   </td>
                 </tr>
               </table>
